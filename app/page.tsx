6cbce7624873385d58193458d5e18266c9a99acf
Sebastien Kempf <skempf@deezer.com>
2023-06-17 09:51:13 +0200

    feat: add head component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,22 @@
+import Head from "next/head";
 import { Landing } from "./components/Landing";
 
 export default function Home() {
-  return <Landing />;
+  return (
+    <>
+      <Head>
+        <title>VinoMemo - Better wine tasting notes</title>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="VinoMemo is a wine tasting notes app. It helps you take notes quickly and easily by guiding you all the way."
+        />
+        <meta name="keywords" content="wine,notes,tasting" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="canonical" href="https://vinomemo-next.vercel.app/" />
+      </Head>
+      <Landing />
+    </>
+  );
 }
