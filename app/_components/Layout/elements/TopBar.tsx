19ab1e2ea6f813781d3724d09d9c384cfcdf9e02
Sebastien Kempf <skempf@deezer.com>
2023-07-03 22:06:26 +0200

    fix: display auth button on unlogged pages

--- app/_components/Layout/elements/TopBar.tsx
+++ app/_components/Layout/elements/TopBar.tsx
@@ -3,9 +3,11 @@
 import { Button, Heading, HStack, useTheme } from "@chakra-ui/react";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export const TopBar = () => {
   const theme = useTheme();
+  const pathname = usePathname();
 
   return (
     <HStack
@@ -32,14 +34,27 @@ export const TopBar = () => {
           Memo
         </Heading>
       </HStack>
-      {/* TODO: Display buttons on unlogged pages */}
       <HStack spacing={{ base: 2, md: 4 }}>
-        <Button as={Link} href="/login" size={{ base: "md", md: "lg" }}>
-          Login
-        </Button>
-        <Button as={Link} href="/signup" size={{ base: "md", md: "lg" }}>
-          Signup
-        </Button>
+        {pathname === "/" && (
+          <>
+            <Button as={Link} href="/login" size={{ base: "md", md: "lg" }}>
+              Login
+            </Button>
+            <Button as={Link} href="/signup" size={{ base: "md", md: "lg" }}>
+              Signup
+            </Button>
+          </>
+        )}
+        {pathname === "/notes" && (
+          <>
+            {/* <Button as={Link} href="/profile" size={{ base: "md", md: "lg" }}>
+              Profile
+            </Button> */}
+            <Button as={Link} href="/logout" size={{ base: "md", md: "lg" }}>
+              Logout
+            </Button>
+          </>
+        )}
       </HStack>
     </HStack>
   );
