c810f61a4dcf4dcbed6809a486d62d2efd5c8d89
Sebastien Kempf <skempf@deezer.com>
2023-07-24 20:41:53 +0200

    wip: simplify logout

--- app/logout/page.tsx
+++ app/logout/page.tsx
@@ -1,19 +1,25 @@
 "use client";
 
 import { useEffect } from "react";
-import { Spinner } from "@chakra-ui/react";
-import { UserLayout } from "../_components/Layout/UserLayout";
-import { useLogout } from "../_hooks/useLogout";
 import { useRouter } from "next/navigation";
+import { UserLayout } from "../_components/Layout/UserLayout";
 
 export default function Logout() {
-  const { isLoading, logoutUser } = useLogout();
   const router = useRouter();
 
   useEffect(() => {
+    const logoutUser = async () => {
+      try {
+        await fetch("/api/logout", {
+          method: "POST",
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    };
     logoutUser();
     router.push("/login");
-  }, [logoutUser, router]);
+  }, [router]);
 
-  return <UserLayout>{isLoading && <Spinner color="brand.900" />}</UserLayout>;
+  return <UserLayout></UserLayout>;
 }
